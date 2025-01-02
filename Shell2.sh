b2a3a463cde24fa2f2d0d4ce304cb410254bb04f
Shazidul Alam <143749400+Shazid5000@users.noreply.github.com>
2025-01-03 04:01:38 +0600

    Add files via upload

---
+++ Shell2.sh
@@ -0,0 +1,6 @@
+read -p "Enter two numbers: " n1 n2
+echo "The numbers are $n1 & $n2"
+echo "Addition is $(expr $n1 + $n2)"
+echo "Subtraction is $(expr $n1 - $n2)"
+echo "Multiplication is $(expr $n1 \* $n2)"
+echo "Division is $(expr $n1 / $n2)"
